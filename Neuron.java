223f626057e377c8be00f47b2d6980fae8e01764
Som3on31 <saharit.f4@gmail.com>
2022-07-18 13:37:58 +0700

    Updated Neuron and removed custom lib

--- Neuron.java
+++ Neuron.java
@@ -1,25 +1,82 @@
-public class Neuron<F,S>{
+/*
+ * @author AR
+ * 
+ * <p>This class is created to make a full use of the concept of a neuron as a unit
+ * </p>
+ */
 
+public class Neuron {
 
-    private int bias;
+    private Neuron[] inputNeurons;
+    private float[] rawInput;
+    private int inputCount;
+    private int MAX_SIZE;
+
+    private float bias;
     // private int weight;
 
-    public Neuron(int inputCount,int outputCount,int bias){
+    public Neuron(int inputCount, boolean rawInputAllowed, int bias) {
+        if (rawInputAllowed)
+            rawInput = new float[inputCount];
+        else
+            inputNeurons = new Neuron[inputCount];
+        MAX_SIZE = inputCount;
+        this.inputCount = 0;
+        MAX_SIZE = inputCount;
 
-        
         this.bias = bias;
     }
 
-    public int getOutput(){
-        
+    public float getOutput() {
+        float totalValue = 0;
+        if (rawInput != null)
+            for (float value : rawInput)
+                totalValue += value;
 
+        if (inputNeurons != null)
+            for (Neuron neuron : inputNeurons)
+                totalValue += neuron.getOutput();
 
-
-        return bias;
+        return totalValue + bias;
     }
 
-    public void getInput(){
+    public boolean addInput(float input) {
+        if (rawInput == null) {
+            System.out.println("Error: this neuron accepts neurons as an input only.");
+            return false;
+        }
+        if (isFull()) {
+            rawInput[inputCount] = input;
+            inputCount++;
 
+            System.out.println("Successfully added " + input);
+            return true;
+        }
+        System.out.println("Error: The input array is full.");
+        return false;
     }
 
+    public boolean addInput(Neuron input) {
+        if (rawInput == null) {
+            System.out.println("Error: this neuron accepts raw inputs as an input only.");
+            return false;
+        }
+        if (isFull()) {
+            inputNeurons[inputCount] = input;
+            inputCount++;
+
+            System.out.println("Successfully added " + input);
+            return true;
+        }
+        System.out.println("Error: The input array is full.");
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return inputCount == 0;
+    }
+
+    public boolean isFull() {
+        return inputCount == MAX_SIZE;
+    }
 }
