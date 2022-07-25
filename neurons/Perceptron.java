e37aa1013bfb7c289fad8f3ce8ab31232c0d233e
Som3on31 <saharit.f4@gmail.com>
2022-07-25 09:03:42 +0700

    Added javadoc and 2 more fns

--- neurons/Perceptron.java
+++ neurons/Perceptron.java
@@ -3,17 +3,29 @@ package neurons;
 public interface Perceptron{
     boolean addInput(Perceptron neuron);
     boolean addInput(float input);
+    float changeInput(int pos,float newInput);
     float getOutput();
 
     boolean isEmpty();
+    /**
+     * Checks if there is a space available for more inputs
+     * @return a boolean when the condition is met.
+     */
     boolean isFull();
 
     /**
      * Returns error
-     * @param expected
-     * @return 
+     * @param expected a value one wishes to see
+     * @param float a value gathered from using a perceptron
+     * @return a number of float number in (WIP)
      */
     float getError(float result,float expected);
 
+    /**
+     * This function is used to determine the final result of an output from a perceptron
+     * @param output a value gathered from using a perceptron
+     * @return
+     */
     float useActivationFn(float output);
+    int useDerivFn(float output);
 }
